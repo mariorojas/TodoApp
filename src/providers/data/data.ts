ba282bcc19474e851282b572ee0613d059ff1f67
Mario Rojas <mario.rojas@enapsys.com>
2018-10-01 09:28:45 -0500

    To-Do base code

---
+++ src/providers/data/data.ts
@@ -0,0 +1,27 @@
+import { Storage } from '@ionic/storage';
+//import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the DataProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class DataProvider {
+
+  //constructor(public http: HttpClient) {
+  constructor(public storage: Storage) {
+    console.log('Hello DataProvider Provider');
+  }
+  
+  getData() {
+	return this.storage.get('todos');
+  }
+  
+  save(data) {
+	this.storage.set('todos', data);
+  }
+
+}
